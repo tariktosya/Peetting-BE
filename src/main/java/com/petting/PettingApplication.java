280b6d3d2fc395d5bb889b8efc566ede559108fe
tariktosya <tarik.tosya@gmail.com>
2023-07-11 14:46:31 +0300

    *

---
+++ src/main/java/com/petting/PettingApplication.java
@@ -0,0 +1,13 @@
+package com.petting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PettingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PettingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/petting/PettingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PettingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
